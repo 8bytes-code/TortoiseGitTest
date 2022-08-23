c16472a5bdc9faa20913630711889020e8f09b6d
8bytes-code <653897636@qq.com>
2022-08-23 23:22:21 +0800

    初始网络框架

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -4,6 +4,7 @@
 #include "pch.h"
 #include "framework.h"
 #include "RemoteCtrl.h"
+#include "ServerSocket.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -16,33 +17,45 @@ CWinApp theApp;
 
 using namespace std;
 
-int main()
-{
-    int nRetCode = 0;
+int main() {
+	int nRetCode = 0;
 
-    HMODULE hModule = ::GetModuleHandle(nullptr);
+	HMODULE hModule = ::GetModuleHandle(nullptr);
 
-    if (hModule != nullptr)
-    {
-        // 初始化 MFC 并在失败时显示错误
-        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
-        {
-            // TODO: 在此处为应用程序的行为编写代码。
-            wprintf(L"错误: MFC 初始化失败\n");
-            nRetCode = 1;
-        }
-        else
-        {
-            // TODO: 在此处为应用程序的行为编写代码。
+	if (hModule != nullptr) {
+		// 初始化 MFC 并在失败时显示错误
+		if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0)) {
+			// TODO: 在此处为应用程序的行为编写代码。
+			wprintf(L"错误: MFC 初始化失败\n");
+			nRetCode = 1;
+		} else {
+			// TODO: 在此处为应用程序的行为编写代码。
+			CServerSocket* pserver = CServerSocket::getInstance();
+			int count = 0;
 
-        }
-    }
-    else
-    {
-        // TODO: 更改错误代码以符合需要
-        wprintf(L"错误: GetModuleHandle 失败\n");
-        nRetCode = 1;
-    }
+			if (pserver->InitSocket() == false) {
+				MessageBox(NULL, _T("网络初始化异常，未能成功初始化，请检查网络状况！"), _T("网络初始化失败"), MB_OK | MB_ICONERROR);
+				exit(0);
+			}
+			while (CServerSocket::getInstance() != NULL) {
+				if (pserver->AcceptClient() == false) {
+					if (count <= 3) {
+						MessageBox(NULL, _T("多次重连仍无法接入，结束本程序！"), _T("接入用户失败"), MB_OK | MB_ICONERROR);
+						exit(0);
+					}
+					MessageBox(NULL, _T("无法正常接入用户，请稍后正在自动重试！"), _T("接入用户失败"), MB_OK | MB_ICONERROR);
+					count++;
+				}
 
-    return nRetCode;
+				int ret = pserver->DealCommand();
+				//
+			}
+		}
+	} else {
+		// TODO: 更改错误代码以符合需要
+		wprintf(L"错误: GetModuleHandle 失败\n");
+		nRetCode = 1;
+	}
+
+	return nRetCode;
 }
