25fd83176c17846e0cc616808dd96af54f7cba8e
8bytes-code <653897636@qq.com>
2022-10-13 22:28:24 +0800

    1.重新设计了sendmessage接口 2.完成了CClientController开发工作

---
+++ RemoteCtrl/RemoteClient/ClientController.cpp
@@ -0,0 +1,119 @@
+#include "pch.h"
+#include "ClientController.h"
+
+
+//静态成员在头文件没有只有声明没有实现，故此要在源文件初始化
+//除此之外，m_instance是唯一的对象接口，不能再每次构造都被重置
+std::map<UINT, CClientController::MSGFUNC> CClientController::m_mapFunc;
+CClientController* CClientController::m_instance = NULL;
+
+CClientController* CClientController::getInstance() {
+	if (m_instance == NULL) {
+		m_instance = new CClientController();
+		struct {
+			UINT nMsg;
+			MSGFUNC func;
+		}MsgFuncs[]={
+			{WM_SEND_PACK, &CClientController::OnSendPack},
+			{WM_SEND_DATA, &CClientController::OnSendData},
+			{WM_SHOW_STATUS, &CClientController::OnShowStatus},
+			{WM_SHOW_WATCH, &CClientController::OnShowWatch},
+			{(UINT)-1, NULL}
+		};
+
+		for (int i = 0; MsgFuncs[i].func != NULL; i++) {
+			m_mapFunc.insert(std::pair<UINT, MSGFUNC>(MsgFuncs[i].nMsg, MsgFuncs[i].func));
+		}
+	}
+	return nullptr;
+}
+
+int CClientController::InitController() {
+	m_hThread = (HANDLE)_beginthreadex(
+		NULL, 0, &CClientController::threadEntry,
+		this, 0, &m_nThreadID
+	);	//CreateThread
+
+	m_statusDlg.Create(IDD_DLG_STATUS, &m_remoteDlg);
+
+	return 0;
+}
+
+int CClientController::Invoke(CWnd*& pMainWnd) {
+	pMainWnd = &m_remoteDlg;
+	return m_remoteDlg.DoModal();
+}
+
+LRESULT CClientController::SendMessage(MSG msg) {
+	HANDLE hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+	if (hEvent == NULL)return-2;
+	MSGINFO info(msg);
+	PostThreadMessage(m_nThreadID, WM_SEND_MESSAGE, (WPARAM)&info, (LPARAM)hEvent);
+	
+	//一直等待信号
+	WaitForSingleObject(hEvent, -1);
+	
+	return info.result;
+}
+
+void CClientController::threadFunc() {
+	MSG msg;
+	while (::GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+
+		if (msg.message == WM_SEND_MESSAGE) {
+			MSGINFO* pmsg = (MSGINFO*)msg.wParam;
+			HANDLE hEvent = (HANDLE)msg.lParam;
+
+			//老样子消息循环队列完事就给他挪出去
+			std::map<UINT, MSGFUNC>::iterator it = m_mapFunc.find(pmsg->msg.message);
+			if (it != m_mapFunc.end()) {
+				pmsg->result = (this->*it->second)(pmsg->msg.message, pmsg->msg.wParam, pmsg->msg.lParam);
+			} else {
+				//-1表示不存在
+				pmsg->result = -1;
+			}
+			SetEvent(hEvent);
+		} else {
+			//没有对应消息则不处理
+			std::map<UINT, MSGFUNC>::iterator it = m_mapFunc.find(msg.message);
+			if (it != m_mapFunc.end()) {
+				(this->*it->second)(msg.message, msg.wParam, msg.lParam);
+			}
+		}
+
+	}
+}
+
+unsigned __stdcall CClientController::threadEntry(void* arg) {
+	CClientController* thiz = (CClientController*)arg;
+	thiz->threadFunc();
+	_endthreadex(0);
+
+	return 0;
+}
+
+LRESULT CClientController::OnSendPack(UINT nMsg, WPARAM wParam, LPARAM lParam) {
+	return LRESULT();
+}
+
+LRESULT CClientController::OnSendData(UINT nMsg, WPARAM wParam, LPARAM lParam) {
+	return LRESULT();
+}
+
+LRESULT CClientController::OnShowStatus(UINT nMsg, WPARAM wParam, LPARAM lParam) {
+	return m_statusDlg.ShowWindow(SW_SHOW);
+}
+
+LRESULT CClientController::OnShowWatch(UINT nMsg, WPARAM wParam, LPARAM lParam) {
+	return m_watchDlg.DoModal();
+}
+
+/*
+* 准备了事件、消息、result
+* 通过PostThreadMessage发送到消息循环，等待result填充内容，同时wait等待setEvent
+* event执行完成继续等待下一个消息
+* 设计小结：因为由线程启动，消息执行顺序有一定出入，
+  但是需要知道是否完成，所以需要事件做处理
+*/
