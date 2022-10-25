8be5226b7604d573575d75e9cf2627b3aab1baa0
8bytes-code <653897636@qq.com>
2022-10-25 15:00:41 +0800

    1.一个吻完成端口的小例程

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -7,6 +7,7 @@
 #include "ServerSocket.h"
 #include "HeTool.h"
 #include "Command.h"
+#include <conio.h>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -20,6 +21,15 @@
 
 //_T("C:\\Windows\\SysWOW64\\RemoteCtrl.exe")
 #define INVOKE_PATH _T("C:\\Users\\test\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RemoteCtrl.exe")
+#define IOCP_LIST_EMPTY 0
+#define IOCP_LIST_PUSH 1
+#define IOCP_LIST_POP 2
+
+enum {
+	IocpListEmpty,
+	IocpListPush,
+	IocpListPop
+};
 
 // 唯一的应用程序对象
 CWinApp theApp;
@@ -51,7 +61,96 @@ bool ChooseAutoInvoke(const CString& strPath) {
 	return true;
 }
 
+typedef struct IocpParm {
+	int nOperator;					//操作
+	std::string strData;			//数据
+	_beginthread_proc_type cbFunc;	//回调
+
+	IocpParm(int op, const char* sData, _beginthread_proc_type cb = NULL) {
+		nOperator = op;
+		strData = sData;
+		cbFunc = cb;
+	}
+	IocpParm() {
+		nOperator = -1;
+	}
+}IOCP_PARAM;
+
+void threadQueueEntry(HANDLE hIOCP) {
+	std::list<std::string> lstString;
+	//获取完成端口状态
+	DWORD dwTransferred = 0;
+	ULONG_PTR CompletionKey = 0;
+	OVERLAPPED* pOverlapped = NULL;
+	while (GetQueuedCompletionStatus(hIOCP, &dwTransferred, &CompletionKey, &pOverlapped, INFINITE)) {
+		if ((dwTransferred == 0) && (CompletionKey == NULL)) {
+			printf("thread is prepare to exit!\r\n");
+			break;
+		}
+		IOCP_PARAM* pParam = (IOCP_PARAM*)CompletionKey;
+		if (pParam->nOperator == IocpListPush) {
+			lstString.push_back(pParam->strData);
+		}
+		else if (pParam->nOperator == IocpListPop) {
+			std::string* pStr = NULL;
+			if (lstString.size() > 0) {
+				pStr = new std::string(lstString.front());
+				lstString.pop_front();
+			}
+			if (pParam->cbFunc) {
+				pParam->cbFunc(pStr);
+			}
+		}
+		else if (pParam->nOperator == IocpListEmpty) {
+			lstString.clear();
+		}
+		delete pParam;
+	}
+	
+	_endthread();
+}
+
+void func(void* arg) {
+	std::string* pstr = (std::string*)arg;
+	if (pstr != NULL) {
+		printf("pop from list:%s\r\n", pstr->c_str());
+		delete pstr;
+	}
+	else {
+		printf("list is empty,not data!\r\n");
+	}
+}
+
 int main() {
+	if (!CHeTool::Init()) return 1;
+	printf("press any key to exit...\r\n");
+	//创建iocp队列
+	HANDLE hIOCP = INVALID_HANDLE_VALUE;	//Input/Output Completion Port
+	hIOCP = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, NULL, 1);
+	HANDLE hThread = (HANDLE)_beginthread(threadQueueEntry, 0, hIOCP);
+	
+	ULONGLONG tick = GetTickCount64();
+	while(_kbhit() != 0){
+		//完成端口把请求和实现分离
+		if (GetTickCount64() - tick > 1300) {
+			PostQueuedCompletionStatus(hIOCP, sizeof(IOCP_PARAM), (ULONG_PTR)new IOCP_PARAM(IocpListPop, "hello world"), NULL);
+		}
+		if (GetTickCount64() - tick > 2000) {
+			PostQueuedCompletionStatus(hIOCP, sizeof(IOCP_PARAM), (ULONG_PTR)new IOCP_PARAM(IocpListPush, "hello world"), NULL);
+			tick = GetTickCount64();
+		}
+		Sleep(1);
+	}
+	if (hIOCP != NULL) {
+		//不为空需要唤醒完成端口启动
+		PostQueuedCompletionStatus(hIOCP, 0, NULL, NULL);
+		WaitForSingleObject(hThread, INFINITE);
+	}
+	CloseHandle(hIOCP);
+	printf("exit done!\r\n");
+	exit(0);
+
+	/*
 	if (CHeTool::IsAdmin()) {
 		if (!CHeTool::Init()) return 1;
 		if (ChooseAutoInvoke(INVOKE_PATH)) {
@@ -75,6 +174,6 @@ int main() {
 			return 1;
 		}
 	}
-
+	*/
 	return 0;
 }
